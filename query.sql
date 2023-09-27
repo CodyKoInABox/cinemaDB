8657bbccdf7d2c5ad3149ed02263bf5b2cff7e9b
Cody Ko In A Box <codykoinabox@gmail.com>
2023-09-27 02:05:48 -0300

    Query dos exercicios 6 e 7

--- query.sql
+++ query.sql
@@ -32,3 +32,27 @@ ON f.cod_filme = e.cod_filme
 INNER JOIN ator a
 ON e.cod_ator = a.cod_ator
 WHERE a.nome = "jim carrey";
+
+--@block EXERCICIO 6
+SELECT nome AS Nome
+FROM ator a
+LEFT JOIN elenco e
+ON a.cod_ator = e.cod_ator
+WHERE e.cod_ator IS NULL;
+
+--@block Query para que o exericio 6 retorne algo (por padrao nesse banco todos os atores participam de algum filme)
+INSERT INTO ator VALUES(
+    12,
+    61, 
+    "thomas cruise",
+    "americano"
+);
+
+--@block EXERCICIO 7
+SELECT f.cod_filme AS Codigo, f.tituloP AS Nome
+FROM filme f
+INNER JOIN elenco e
+ON f.cod_filme = e.cod_filme
+LEFT JOIN ator a 
+ON e.cod_ator = a.cod_ator
+WHERE a.nome != "tom hanks";
