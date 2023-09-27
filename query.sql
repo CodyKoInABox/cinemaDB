a09c6989fea0641cec8efa32d5785d76776a094b
Cody Ko In A Box <codykoinabox@gmail.com>
2023-09-27 12:08:11 -0300

    Query dos exercicios 8, 9, 10 e 11

--- query.sql
+++ query.sql
@@ -1,14 +1,16 @@
-
-
 --@block
 SELECT * FROM filme;
+
+--@block
+SELECT * from elenco;
+
+--@block
+SELECT * from ator;
 
 --@block EXERCICIO 1
 SELECT tituloP AS Titulo, duracao AS Duracao
 FROM filme;
 
---@block
-SELECT * from ator;
 
 --@block EXERCICIO 2
 SELECT cod_ator AS Codigo, nome AS Nome
@@ -22,9 +24,6 @@ FROM ator;
 --@block EXERCICIO 4
 SELECT nome AS Nome, idade AS Idade, nacionalidade AS Nacionalidade
 FROM ator;
-
---@block
-SELECT * from elenco;
 
 --@block EXERCICIO 5
 SELECT tituloO AS Nome FROM filme f INNER JOIN elenco e
@@ -56,3 +55,38 @@ ON f.cod_filme = e.cod_filme
 LEFT JOIN ator a 
 ON e.cod_ator = a.cod_ator
 WHERE a.nome != "tom hanks";
+
+--@block EXERCICIO 8
+SELECT COUNT(e.cod_ator) AS Qtd_Filmes
+FROM elenco e
+INNER JOIN ator a
+ON e.cod_ator = a.cod_ator
+WHERE a.nome = "george clooney";
+
+--@block EXERCICIO 9
+SELECT a.nome AS Nome
+FROM ator a
+INNER JOIN elenco e
+ON a.cod_ator = e.cod_ator
+INNER JOIN filme f
+ON f.cod_filme = e.cod_filme
+WHERE f.tituloP = "onze homens e um segredo";
+
+--@block EXERCICIO 10
+SELECT SUM(f.duracao) AS Soma_Duracao
+FROM filme f
+INNER JOIN elenco e
+ON f.cod_filme = e.cod_filme
+INNER JOIN ator a
+ON e.cod_ator = a.cod_ator
+WHERE a.nome = "angelina jolie";
+
+--@block EXERCICIO 11
+SELECT f.tituloO AS Nome
+FROM filme f
+INNER JOIN elenco e
+ON f.cod_filme = e.cod_filme
+INNER JOIN ator a
+ON e.cod_ator = a.cod_ator
+WHERE a.nome = "brad pitt"
+AND f.pais_origem = "estados unidos";
